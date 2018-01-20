e22c421c2cfc2a73c9062018f9f73429ab185289
kovali <irynakovalenko03@gmail.com>
2018-01-20 22:49:27 +0200

    comment

---
+++ 10.php
@@ -0,0 +1,16 @@
+<?php
+$day = 2;
+switch ($day) {
+    case 1:
+        echo "Это рабочий день 1";
+        break;
+    case 2:
+        echo "Это рабочий день 2";
+        break;
+    case 3:
+        echo "Это рабочий день 3";
+        break;
+    case 4: echo "Это рабочий день 4";
+        break;
+    case 5: echo "Это рабочий день 5";
+        break;    }
